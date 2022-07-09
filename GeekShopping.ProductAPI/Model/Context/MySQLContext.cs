70f4b73548728f0ba7064b015059884e8fdabd11
LucasPenha <lucaspenha3@gmail.com>
2022-07-09 15:15:53 -0300

    commit context ramo teste

--- GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -8,5 +8,22 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
         public DbSet<Product> Products { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            for(int i = 3; i <= 20; i++)
+            {
+                _ = modelBuilder.Entity<Product>().HasData(new Product
+                {
+                    Id = i,
+                    Name = "Produto " + i.ToString(),
+                    Price = Math.Round(Convert.ToDecimal(10 * i), 2),
+                    CategoryName = "Categoria produto " + i.ToString(),
+                    image_url = "http://teste.com",
+                    Description = "Descrição do produto " + i.ToString()
+                });
+            }
+            
+        }
+
     }
 }
